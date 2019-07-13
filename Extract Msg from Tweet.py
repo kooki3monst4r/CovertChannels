ce14b598ef102d360b64f0fa61d7348351839b09
Olivia Flaherty <okflaherty@live.co.uk>
2019-07-13 13:46:18 -0400

    Added a file containing the code to extract the binary message out of the tweets and inserts it into msg.txt Now works with the hash tag appended to the message

--- Extract Msg from Tweet.py
+++ Extract Msg from Tweet.py
@@ -1,22 +1,25 @@
 
 # Requires tweets to be in an array
-tweet_array = ['*nuzzles*', 'goodmorning.', 'hello!', 'Goodbye.', 'where are you!', 'the end']
-tweet_extract = ' '
+tweet_array = ['*nuzzles*#12345', 'goodmorning.#12345', 'hello!#12345', 'Goodbye.#12345', 'where are you!#12345', 'the end#12345']
+tweet_extract = '0'
 
 # For loop to extract the last character of each sentence.
 # The start of the message is identfied with the tweet ending with the '*' character.
 # Tweets that end with a . or ! will be added to tweet_extract
 # The message will end when the last character of the tweet is not any of the three above characters.
 
+
 for word in tweet_array:
-    if word[-1:] == '*':
+    if word[-7:-6] == '*':
         f= open('msg.txt', 'a+')
         f.write(' Start msg ')
         f.close()
-    elif word[-1:] == '.':
+    elif word[-7:-6] == '.':
         tweet_extract = tweet_extract + '0'
-    elif word[-1:] == '!':
+        print(tweet_extract)
+    elif word[-7:-6] == '!':
         tweet_extract = tweet_extract + '1'
+        print(tweet_extract)
     else:
         break
 
