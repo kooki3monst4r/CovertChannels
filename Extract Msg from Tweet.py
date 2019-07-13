ed5a83b15a7d343a28d9d996c91b2a4dca4307d8
Olivia Flaherty <okflaherty@live.co.uk>
2019-07-13 11:14:51 -0400

    1. Added the code to make a text file into a binary array. 2. Added a file containing the code to extract the binary message out of the tweets and inserts it into msg.txt

--- Extract Msg from Tweet.py
+++ Extract Msg from Tweet.py
@@ -1,11 +1,22 @@
 
 # Requires tweets to be in an array
-tweet_array = ['goodmorning.', 'hello!', 'Goodbye.', 'where are you!', 'the end']
-tweet_extract = ''
-# For loop to extract the last character of each sentence.  This will stop when the character is not a ! or a .
+tweet_array = ['*nuzzles*', 'goodmorning.', 'hello!', 'Goodbye.', 'where are you!', 'the end']
+tweet_extract = ' '
+
+# For loop to extract the last character of each sentence.
+# The start of the message is identfied with the tweet ending with the '*' character.
+# Tweets that end with a . or ! will be added to tweet_extract
+# The message will end when the last character of the tweet is not any of the three above characters.
+
 for word in tweet_array:
-    if word[-1:] == '.' or  word[-1:] == '!':
-        tweet_extract = tweet_extract + word[-1:]
+    if word[-1:] == '*':
+        f= open('msg.txt', 'a+')
+        f.write(' Start msg ')
+        f.close()
+    elif word[-1:] == '.':
+        tweet_extract = tweet_extract + '0'
+    elif word[-1:] == '!':
+        tweet_extract = tweet_extract + '1'
     else:
         break
 
@@ -14,12 +25,13 @@ def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
     n = int(bits, 2)
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
 
-# Replace all ! with 1 and all . with 0
-tweet_extract_one = (tweet_extract.replace('!', '1'))
-tweet_extract_final = tweet_extract_one.replace('.', '0')
-
 # convert binary to text
-original_input_file = text_from_bits(tweet_extract_final)
+original_input_file = text_from_bits(tweet_extract)
+
+# Append converted message to our text file
+f=open('msg.txt', 'a+')
+f.write(original_input_file + ' end msg ')
+f.close()
 
 # print out of secret message
 print(original_input_file)
